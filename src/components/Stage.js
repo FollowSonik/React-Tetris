f78f2789b30cca346ba00d03c3a74101b26f9a96
FollowSonik <followsonik@gmail.com>
2020-07-31 07:56:03 +0700

    I think it fixed, but not sure! Just misspelled the .length instead of .height.

--- src/components/Stage.js
+++ src/components/Stage.js
@@ -4,7 +4,7 @@ import Cell from './Cell';
 
 export default function ({ stage }) {
   return (
-    <StyledStage width={stage[0].length} height={stage.height}>
+    <StyledStage width={stage[0].length} height={stage.length}>
       {stage.map(row => row.map((cell, x) => {
         return <Cell key={x} type={cell[0]} />
       }))}
