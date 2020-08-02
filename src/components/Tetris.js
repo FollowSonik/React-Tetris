0efb7aabb99996654632acc2750308684c93b323
FollowSonik <followsonik@gmail.com>
2020-08-02 08:56:47 +0700

    Rotation!

--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -14,7 +14,7 @@ export default function () {
   const [dropTime, setDropTime] = useState(null);
   const [gameOver, setGameOver] = useState(false);
 
-  const [player, updatePlayerPos, resetPlayer] = usePlayer();
+  const [player, updatePlayerPos, resetPlayer, playerRotate] = usePlayer();
   const [stage, setStage] = useStage(player, resetPlayer);
 
   function movePlayer(dir) {
@@ -51,6 +51,7 @@ export default function () {
       if (keyCode === 37) movePlayer(-1);
       else if (keyCode === 39) movePlayer(1);
       else if (keyCode === 40) dropPlayer();
+      else if (keyCode === 38) playerRotate(stage, 1);
     }
   }
 
--- src/hooks/usePlayer.js
+++ src/hooks/usePlayer.js
@@ -1,7 +1,7 @@
 import { useState, useCallback } from 'react';
 
 import { TETROMINOS, randomization } from '../tetrominos';
-import { STAGE_WIDTH } from '../gameHelpers';
+import { STAGE_WIDTH, checkCollision } from '../gameHelpers';
 
 export default function () {
   const [player, setPlayer] = useState({
@@ -9,6 +9,33 @@ export default function () {
     tetromino: TETROMINOS[0].shape,
     collided: false,
   });
+
+  function rotate(matrix, dir) {
+    const rotatedTetro = matrix.map((_, i) => {
+      return matrix.map(col => col[i]);
+    });
+
+    if (dir > 0) return rotatedTetro.map(row => row.reverse());
+    return rotatedTetro.reverse();
+  }
+
+  function playerRotate(stage, dir) {
+    const clonedPlayer = JSON.parse(JSON.stringify(player));
+    clonedPlayer.tetromino = rotate(clonedPlayer.tetromino, dir);
+
+    const pos = clonedPlayer.pos.x;
+    let offset = 1;
+    while (checkCollision(clonedPlayer, stage, { x: 0, y: 0 })) {
+      clonedPlayer.pos.x += offset;
+      offset = -(offset + (offset > 0 ? 1 : -1));
+      if (offset > clonedPlayer.tetromino[0].length) {
+        rotate(clonedPlayer.tetromino, -dir);
+        return clonedPlayer.pos.x = pos;
+      }
+    }
+
+    setPlayer(clonedPlayer);
+  }
 
   function updatePlayerPos({ x, y, collided }) {
     setPlayer(prev => ({
@@ -32,5 +59,5 @@ export default function () {
     });
   }, []);
 
-  return [player, updatePlayerPos, resetPlayer];
+  return [player, updatePlayerPos, resetPlayer, playerRotate];
 } 
