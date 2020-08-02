9fe1d69edfd891d0b94e4d5d470edb7c570edd0c
FollowSonik <followsonik@gmail.com>
2020-08-02 09:17:22 +0700

    rowsCleared.

--- src/hooks/useStage.js
+++ src/hooks/useStage.js
@@ -3,8 +3,24 @@ import { createStage } from '../gameHelpers';
 
 export default function (player, resetPlayer) {
   const [stage, setStage] = useState(createStage());
+  const [rowsCleared, setRowsCleared] = useState(0);
 
   useEffect(() => {
+    setRowsCleared(0);
+
+    function sweepRows(newStage) {
+      return newStage.reduce((acc, row) => {
+        const condition = row.findIndex(cell => cell[0] === 0) === -1;
+        if (condition) {
+          setRowsCleared(prev => prev + 1);
+          acc.unshift(new Array(newStage[0].length).fill([0, 'clear']));
+          return acc;
+        };
+        acc.push(row);
+        return acc;
+      }, []);
+    }
+
     function updateStage(prevStage) {
       const newStage = prevStage.map(row => {
         return row.map(cell => {
@@ -23,7 +39,10 @@ export default function (player, resetPlayer) {
         });
       });
 
-      if (player.collided) resetPlayer();
+      if (player.collided) {
+        resetPlayer();
+        return sweepRows(newStage);
+      }
 
       return newStage;
     }
@@ -31,5 +50,5 @@ export default function (player, resetPlayer) {
     setStage(prev => updateStage(prev));
   }, [player, resetPlayer]);
 
-  return [stage, setStage];
+  return [stage, setStage, rowsCleared];
 }
